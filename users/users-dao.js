ceed5d643e660e692790c51559e412e73c526e51
whl961210 <whl961210@gmail.com>
2023-08-08 19:50:39 +0800

    fixed update function

--- users/users-dao.js
+++ users/users-dao.js
@@ -30,11 +30,13 @@ export const findUserByCredentials = (username, password) => {
 export const createUser = (user) => users.push(user);
 
 
-export const updateUser = (uid, user) => {
- const index = users.findIndex((u) => u._id === uid);
- users[index] = { ...users[index], ...user };
- return {status: 'ok'}
-};
+export const updateUser = (username, user) => {
+    const index = users.findIndex((u) => u.username === username);
+    if (index === -1) return {status: 'error', message: 'User not found'};
+    users[index] = { ...users[index], ...user };
+    return {status: 'ok'};
+  };
+  
 export const deleteUser = (uid) => {
  const index = users.findIndex((u) => u._id === uid);
  users.splice(index, 1);
