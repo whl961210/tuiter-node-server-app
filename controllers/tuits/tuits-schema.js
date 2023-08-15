e9a3d63474d132595ba2ac26ef0942e82992c127
whl961210 <whl961210@gmail.com>
2023-08-15 18:33:50 +0800

    fixed schema function

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -1,7 +1,16 @@
 import mongoose from 'mongoose';
 const schema = mongoose.Schema({
-  tuit: String,
-  likes: Number,
-  liked: Boolean,
-}, {collection: 'tuits'});
+    tuit: String,
+    likes: Number,
+    liked: Boolean,
+    unlikes: Number,
+    username: String,
+    topic: String,
+    handle: String,
+    time: String,
+    image: String,
+    title: String,
+    replies: Number,
+    retuits: Number
+  }, { collection: 'tuits' });
 export default schema;
